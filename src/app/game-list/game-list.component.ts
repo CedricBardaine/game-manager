27fea170213c201e58d9444c0f037d88d36e646a
Cédric B <CedricBardaine@users.noreply.github.com>
2019-12-11 17:07:44 +0100

    ajout d1 jeu

--- src/app/game-list/game-list.component.ts
+++ src/app/game-list/game-list.component.ts
@@ -48,6 +48,13 @@ export class GameListComponent implements OnInit {
        note: 3 ,
        description: 'Version Game Gear pour ma part. Pire jeu de baston de l univers. Il y a 6 personnages, qui sont en fait des déclinaisons de 3 sprites de célèbres dinosaures (tyrannosaure, tricératops et singe). Ca tourne à deux à l heure. Si je dis pas de connerie ils ont chacun 3 attaques différentes, un truc dans le genre. Ils pourraient en avoir 30, ce serait quand même nul, étant donnée la rigidité du jeu, de toute façon... Mais c est pour dire. ' 
       },
+    {
+      nom: 'Half-Life' ,
+      type: 'fps' ,
+      image: 'https://www.numerama.com/content/uploads/2017/07/img_0034.jpg' ,
+       note: 9 ,
+       description: "Half-Life relate les aventures de Gordon Freeman, chercheur au Laboratoire des matériaux anormaux du centre de recherche de Black Mesa, un gigantesque complexe scientifique top secret installé dans une base militaire désaffectée totalement enterrée sous la surface. Alors qu'il participait à une expérience sur un mystérieux échantillon de cristal, Freeman ouvre involontairement une brèche interdimensionnelle vers un monde parallèle, Xen, peuplé de créatures extraterrestres. Des aliens d'espèces et de races diverses font irruption un peu partout dans le centre et attaquent sauvagement son personnel. La salle de test est partiellement détruite par la résonance en chaîne, mais Freeman, équipé d'une combinaison de protection en milieu hostile (HEV), parvient à en sortir indemne après avoir été pris dans une tempête de portails. Après s'être remis d'un court évanouissement, il prend conscience de la catastrophe… " 
+      },
 
     ]; 
     
