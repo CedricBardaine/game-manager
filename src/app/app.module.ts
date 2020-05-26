001895c8f8ed7259b3a80a99142621222b0b8198
Cédric B <cedricbardaine@gmail.com>
2020-05-26 12:17:26 +0200

    make routing works & add NotFoundComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,36 @@ import { GameListComponent } from './game-list/game-list.component';
 import { GameListFilterComponent } from './game-list-filter/game-list-filter.component';
 import { ActionButtonComponent } from './action-button/action-button.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AppRoutingModule } from './app-routing.module';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const appRoutes: Routes = [
+  { path: 'product', component: GameListComponent },
+  { path: 'NotFoundComponent', component: NotFoundComponent },
+  
+  { path: '',   redirectTo: '/product', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     GameListComponent,
     GameListFilterComponent,
-    ActionButtonComponent
+    ActionButtonComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      // {enableTracing: true} 
+      ),
   ],
   providers: [],
   bootstrap: [AppComponent]
